0c2c684652ec660c039fb5766a6992df830175dc
Muhammad Agung Zuhdi <janissaryagung@gmail.com>
2021-07-31 21:29:52 +0700

    Home content update

--- js/pages/home.js
+++ js/pages/home.js
@@ -1,10 +1,10 @@
 const homeMain = `
 <div class="profile-section-wrapper">
-  <img
+    <img
     class="profile-img"
-    src="https://dummyimage.com/250x250/000/fff"
+    src="/img/Potrait.jpg"
     alt="Foto Profil"
-  />
+    />
   <div class="profile-detail-wrapper">
     <h3 class="section-heading">Muhammad Agung Zuhdi</h3>
     <p class="profile-role">Back End Developer</p>
@@ -47,29 +47,56 @@ const homeMain = `
 `;
 const homeAside = `
 <div class="education-section-wrapper">
-          <h3 class="section-heading">Education</h3>
-          <ul class="education-wrapper">
-            <li>
-              <span class="institution-name">IPB University</span>
-              <span class="major">Computer Engineering (2018 - 2021)</span>
-              <span class="degree">Associate's Degree • GPA 3.88</span>
-            </li>
-            <li>
-              <span class="institution-name">SMAN 1 Cikarang Selatan</span>
-              <span class="major">Science (2015 - 2018)</span>
-              <span class="degree">High School</span>
-            </li>
-          </ul>
-        </div>
-        <div class="skill-section-wrapper">
-          <h3 class="section-heading">Skills</h3>
-          <ul class="skill-wrapper">
-            <li>PHP</li>
-            <li>•</li>
-            <li>Phyton</li>
-            <li>C</li>
-            <li>IoT</li>
-          </ul>
-        </div>
+  <h3 class="section-heading">Education</h3>
+  <ul class="education-wrapper">
+    <li>
+      <span class="institution-name">IPB University</span>
+      <span class="major">Computer Engineering (2018 - 2021)</span>
+      <span class="degree">Associate's Degree • GPA 3.88</span>
+    </li>
+    <li>
+      <span class="institution-name">SMAN 1 Cikarang Selatan</span>
+      <span class="major">Science (2015 - 2018)</span>
+      <span class="degree">High School</span>
+    </li>
+  </ul>
+</div>
+<div class="education-section-wrapper">
+  <h3 class="section-heading">Organizations</h3>
+  <ul class="education-wrapper">
+    <li>
+      <span class="institution-name">Karang Taruna Cikarang Selatan</span>
+      <span class="major">Treasurer (2021 - Present)</span>
+    </li>
+    <li>
+      <span class="institution-name">HIMAVO Micro IT Community IPB</span>
+      <span class="major">Web Master Division (2018 - 2020)</span>
+    </li>
+  </ul>
+</div>
+<div class="skill-section-wrapper">
+  <h3 class="section-heading">Programming Languages</h3>
+  <ul class="skill-wrapper">
+    <li><strong>PHP</strong></li>
+    <li>JavaScript</li>
+    <li>C</li>
+    <li>Phyton</li>
+  </ul>
+</div>
+<div class="skill-section-wrapper">
+  <h3 class="section-heading">Tools & Technologies</h3>
+  <ul class="skill-wrapper">
+    <li><strong>Laravel</strong></li>
+    <li>CodeIgniter</li>
+    <li>REST API</li>
+    <li>Git</li>
+    <li>MySQL</li>
+    <li>PostgreSQL</li>
+    <li>Oracle Data Modeller</li>
+    <li>IoT</li>
+    <li>Arduino</li>
+    <li>Microcontroller</li>
+  </ul>
+</div>
 `;
 export { homeMain, homeAside };
