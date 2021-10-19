ef4b14ac1f49913596f55b96c08c1ff697d49a36
MAZuhdi <janissaryagung@gmail.com>
2021-10-19 10:45:32 +0700

    update potrait

--- js/pages/home.js
+++ js/pages/home.js
@@ -2,7 +2,7 @@ const homeMain = `
 <div class="profile-section-wrapper">
     <img
     class="profile-img lazyload"
-    data-src="/img/Potrait4.jpg"
+    data-src="/img/Potrait5.jpg"
     src="/img/placeholder.jpg"
     alt="Foto Profil"
     />
